5603b167100755f10c05e4c7636c0f5b5f9b966d
Syntax <toningamer36023@gmail.com>
2024-08-02 14:40:26 -0300

    Melhorado o design responsivo entre dispositivos, e o formulário para adicionar tarefas agora funciona (Dá para colocar apenas 1 tarefa, por enquanto)

--- scripts.js
+++ scripts.js
@@ -11,5 +11,28 @@ function toggleVisibility() {
         editar.classList.add('visible');
     }
 
-    isVisible = !isVisible; // Alterna o estado
+    isVisible = !isVisible;
 }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const form = document.getElementById('form');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        let titulo = document.getElementById('titulo').value;
+        let descricao = document.getElementById('descricao').value;
+
+        let tTask = document.getElementById('task')
+        let dTask = document.getElementById('desc')
+
+        if (titulo === "" && descricao === "") {
+            alert("Campos vazios!")
+        } else if (titulo === "") {
+            alert("Insira um título")
+        } else {
+            tTask.innerText = titulo
+            dTask.innerText = descricao
+        }
+    });
+});
