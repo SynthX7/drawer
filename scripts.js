e3857c1e5c40dd5c012f15104bbba46dee5067a8
Syntax <toningamer36023@gmail.com>
2024-08-04 09:56:06 -0300

    Pequenas atualizações de design, o código está uma bagunça e cheio de bugs, irei corrigir isso ainda hoje

--- scripts.js
+++ scripts.js
@@ -1,3 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    criar(); // Inicializa a funcionalidade de criação de tarefas
+    renderizarTarefas(); // Renderiza as tarefas ao carregar a página
+
+    // Atualiza a saudação com base na hora
+    atualizarSaudacao();
+
+    // Aplica estilos específicos para mobile, se necessário
+    if (detectMobile()) {
+        aplicarEstilosMobile();
+    }
+});
+
+function atualizarSaudacao() {
+    const saudacao = document.getElementById('saudacoes');
+    const now = new Date();
+    const hour = now.getHours();
+
+    if (hour >= 22) {
+        saudacao.innerText = "Bons sonhos";
+    } else if (hour >= 18) {
+        saudacao.innerText = "Boa noite";
+    } else if (hour >= 12) {
+        saudacao.innerText = "Boa tarde";
+    } else if (hour >= 6) {
+        saudacao.innerText = "Bom dia";
+    } else {
+        saudacao.innerText = "Boa madrugada";
+    }
+}
+
 let isVisible = false; // Controla o estado de visibilidade
 
 function toggleVisibility() {
@@ -16,56 +47,58 @@ function toggleVisibility() {
 
 function criar() {
     const form = document.getElementById('form');
-
     form.addEventListener('submit', function(event) {
         event.preventDefault();
 
-        let titulo = document.getElementById('titulo').value;
-        let descricao = document.getElementById('descricao').value;
+        const titulo = document.getElementById('titulo').value;
+        const descricao = document.getElementById('descricao').value;
 
         if (titulo === "" && descricao === "") {
             alert("Campos vazios!");
-            return; // Não cria uma nova tarefa se os campos estiverem vazios
+            return;
         } else if (titulo === "") {
             alert("Insira um título");
-            return; // Não cria uma nova tarefa se o título estiver vazio
+            return;
         }
 
         const newTask = {
             titulo: titulo,
             descricao: descricao,
-            marcado: false // Adiciona a propriedade marcado para a nova tarefa
+            marcado: false
         };
 
-        let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+        const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
         tarefas.push(newTask);
         localStorage.setItem('tarefas', JSON.stringify(tarefas));
 
-        renderizarTarefas(); // Renderiza todas as tarefas após adicionar uma nova tarefa
-
-        form.reset(); // Limpar os campos após a submissão
+        renderizarTarefas();
+        form.reset();
     });
 }
 
 function renderizarTarefas() {
     const container = document.getElementById('container');
-    container.innerHTML = ''; // Limpa todas as tarefas existentes
+    container.innerHTML = '';
 
-    let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+    const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
     tarefas.forEach((tarefa, index) => {
-        if (tarefa.marcado) return; // Não renderiza tarefas marcadas como removidas
+        if (tarefa.marcado) return;
 
         const newTask = document.createElement('div');
         newTask.className = "tarefa";
+
         const inputNewTask = document.createElement('input');
         inputNewTask.type = "checkbox";
         inputNewTask.checked = tarefa.marcado;
+
         const textsNewTask = document.createElement('div');
         textsNewTask.className = "texto-tarefa";
+
         const titleNewTask = document.createElement('h2');
         titleNewTask.className = "task";
         titleNewTask.textContent = tarefa.titulo;
+
         const descNewTask = document.createElement('p');
         descNewTask.className = "desc";
         descNewTask.textContent = tarefa.descricao;
@@ -78,32 +111,68 @@ function renderizarTarefas() {
 
         container.appendChild(newTask);
 
-        // Adiciona o evento para esconder a tarefa quando o checkbox for marcado
         inputNewTask.addEventListener('change', function() {
-            tarefas[index].marcado = this.checked; // Atualiza o estado da tarefa
+            tarefas[index].marcado = this.checked;
             localStorage.setItem('tarefas', JSON.stringify(tarefas));
-            renderizarTarefas(); // Re-renderiza as tarefas
+            renderizarTarefas();
         });
     });
+
+    if (detectMobile()) {
+        aplicarEstilosMobile();
+    }
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    criar(); // Chama a função criar após o DOM estar carregado
-    renderizarTarefas(); // Renderiza as tarefas quando a página é carregada
-});
+function detectMobile() {
+    return /Mobi|Android/i.test(navigator.userAgent);
+}
 
-const saudacao = document.getElementById('saudacoes');
-const now = new Date();
-const hour = now.getHours();
+function aplicarEstilosMobile() {
+    const body = document.body.style;
+    const tasks = document.getElementsByClassName('tarefa');
+    const checkboxes = document.querySelectorAll('.tarefa input[type="checkbox"]');
+    const texts = document.querySelectorAll('.texto-tarefa');
+    const criarButton = document.querySelector('.criar');
+    const form = document.querySelector('form');
+    const formInputs = form.querySelectorAll('input');
+    const formButton = form.querySelector('button');
 
-if (hour >= 22) {
-    saudacao.innerText = "Bons sonhos";
-} else if (hour >= 18) {
-    saudacao.innerText = "Boa noite";
-} else if (hour >= 12) {
-    saudacao.innerText = "Boa tarde";
-} else if (hour >= 6) {
-    saudacao.innerText = "Bom dia";
-} else {
-    saudacao.innerText = "Boa madrugada";
+    body.fontSize = "10px";
+
+    Array.from(tasks).forEach(task => {
+        task.style.height = "60px";
+        task.style.width = "100%";
+    });
+
+    checkboxes.forEach(checkbox => {
+        checkbox.style.width = "15px";
+        checkbox.style.height = "15px";
+    });
+
+    texts.forEach(text => {
+        text.style.marginTop = "2%";
+        text.style.marginLeft = "10px";
+        text.style.marginBottom = "2%";
+        text.style.fontSize = "12px";
+    });
+
+    criarButton.style.position = "fixed";
+    criarButton.style.width = "40px";
+    criarButton.style.height = "40px";
+    criarButton.style.bottom = "5px";
+    criarButton.style.right = "5px";
+
+    form.style.width = "90%";
+    form.style.maxWidth = "250px";
+    form.style.padding = "10px";
+
+    formInputs.forEach(input => {
+        input.style.padding = "5px";
+        input.style.borderRadius = "10px";
+        input.style.width = "100%";
+    });
+
+    formButton.style.padding = "5px";
+    formButton.style.borderRadius = "10px";
+    formButton.style.marginTop = "5%";
 }
